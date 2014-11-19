6fd17b8d4e29fb87e5dcf12ebd00922f304f3df8
Lana Milley <lana@Lanas-MacBook-Pro.local>
2014-11-19 12:42:00 -0500

    Assignment-Sockets-Boilerplate

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,19 @@
+
+// Wait for DOM to Load
+jQuery(function($) {
+
+    // Create New Socket Connection using Socket.io
+    var socket = io();
+
+    // Send A Message To The Server
+    $('a').on('click', function(){
+      var text = $('input').val();
+      socket.emit('message', text);
+    });
+
+    // Recieve Update Event From The Server
+    socket.on('update', function(msg){
+      $('.messages').append(msg).append('<br>');
+    });
+
+});
